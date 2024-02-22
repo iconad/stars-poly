282a1c9ef04a1df680770d25dbbdc4dc1fe9f1c6
amirehman <30595897+amirehman@users.noreply.github.com>
2024-02-22 12:26:52 +0400

    updated

--- dist/js/app.js
+++ dist/js/app.js
@@ -43,6 +43,104 @@ document.addEventListener("DOMContentLoaded", function (event) {
       /* #################################### */
 
 
+        var elements = document.querySelectorAll(".scroll-counter")
+      
+        elements.forEach(function(item) {
+          // Add new attributes to the elements with the '.scroll-counter' HTML class
+          item.counterAlreadyFired = false
+          item.counterSpeed = item.getAttribute("data-counter-time") / 45
+          item.counterTarget = +item.innerHTML
+          item.counterCount = 0
+          item.counterStep = item.counterTarget / item.counterSpeed
+      
+          item.updateCounter = function() {
+            item.counterCount = item.counterCount + item.counterStep
+            item.innerHTML = Math.ceil(item.counterCount).toLocaleString()
+            item.classList.add('animate')
+      
+            if (item.counterCount < item.counterTarget) {
+              setTimeout(item.updateCounter, item.counterSpeed)
+            } else {
+              item.innerHTML = item.counterTarget.toLocaleString()
+            }
+          }
+        })
+      
+        // Function to determine if an element is visible in the web page
+        var isElementVisible = function isElementVisible(el) {
+          var scroll = window.scrollY || window.pageYOffset
+          var boundsTop = el.getBoundingClientRect().top + 100 + scroll
+          var viewport = {
+            top: scroll,
+            bottom: scroll + window.innerHeight,
+          }
+          var bounds = {
+            top: boundsTop,
+            bottom: boundsTop + el.clientHeight,
+          }
+          return (
+            (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+            (bounds.top <= viewport.bottom && bounds.top >= viewport.top)
+          )
+        }
+      
+        // Funciton that will get fired uppon scrolling
+        var handleScroll = function handleScroll() {
+          elements.forEach(function(item, id) {
+            if (true === item.counterAlreadyFired) return
+            if (!isElementVisible(item)) return
+            item.updateCounter()
+            item.counterAlreadyFired = true
+          })
+        }
+      
+        // Fire the function on scroll
+        window.addEventListener("scroll", handleScroll)
+
+
+        // stars rotating
+        document.querySelectorAll('.star-image').forEach(function(star) {
+          gsap.to(star, {
+            scrollTrigger: {
+              trigger: star,
+              start: 'top center+=500',
+              end: '+=1500',
+              scrub: true,
+            },
+            rotation: 360,
+            ease: 'none'
+          });
+        });
+
+
+        // Marquee effect
+
+        const marqueeInner = document.querySelector('.marquee__inner');
+
+        if(marqueeInner) {
+
+          // Calculate the width of the marquee text
+          const marqueeWidth = marqueeInner.offsetWidth;
+
+          // Function to animate the marquee
+          const animateMarquee = () => {
+            // Reset position to start from the right again
+            gsap.set(marqueeInner, { x: window.innerWidth });
+
+            // Animate to move to the left beyond its own width
+            gsap.to(marqueeInner, {
+              x: -marqueeWidth,
+              duration: 10, // Duration of the animation in seconds, adjust as needed for speed
+              ease: "linear", // Linear animation for a smooth continuous effect
+              onComplete: animateMarquee, // Call the function again to create an infinite loop
+            });
+          };
+
+          // Start the marquee animation
+          animateMarquee();
+
+        }
+
 
       var accordionToggles = document.querySelectorAll('.accordion');
 
@@ -75,9 +173,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
       // Get the navbar and burger icon
         const navbar = document.querySelector('.main-navbar');
         const navbBurger = document.getElementById('navbBurger');
-
-        console.log('navbBurger');
-
         // Add a click event listener to the burger icon
         navbBurger.addEventListener('click', function () {
           // Toggle the class to change the height
@@ -113,45 +208,47 @@ document.addEventListener("DOMContentLoaded", function (event) {
         })
 
 
-        // Marquee effect
-        let black = '#0A0A0A';
-        let offWhite = '#EDEEE9';
 
-        // saveStyles is used because GSAP writes inline CSS for styling.
-        // If we resize our browser, we want the styling of each viewport-based animation to be saved.
 
-        ScrollTrigger.matchMedia({
+
+        
+
+
+        // // saveStyles is used because GSAP writes inline CSS for styling.
+        // // If we resize our browser, we want the styling of each viewport-based animation to be saved.
+
+        // ScrollTrigger.matchMedia({
           
-          // the mobile animations are the same as the desktop, except the font colors are different
-          // the scrub is delayed by 1s with relation to the scrollbar (scrub: 1)
-          // the animation starts a bit earlier
-          "(max-width: 768px)": function() {
+        //   // the mobile animations are the same as the desktop, except the font colors are different
+        //   // the scrub is delayed by 1s with relation to the scrollbar (scrub: 1)
+        //   // the animation starts a bit earlier
+        //   "(max-width: 768px)": function() {
             
-            let mobileTL = gsap.timeline({
-              scrollTrigger: {
-                trigger: ".marquee",
-                start: "+20% bottom",
-                scrub: 1,
-              }
-            });
+        //     let mobileTL = gsap.timeline({
+        //       scrollTrigger: {
+        //         trigger: ".marquee",
+        //         start: "+20% bottom",
+        //         scrub: 1,
+        //       }
+        //     });
 
-            mobileTL.to(".first", {duration: 1, xPercent: -200, color: offWhite})
-          },
+        //     mobileTL.to(".first", {duration: 1, xPercent: -200, color: offWhite})
+        //   },
           
-          // The tablet and desktop animations have a delay of 7s on scrubs (scrub: 2).
-          "(min-width: 769px)": function() {
+        //   // The tablet and desktop animations have a delay of 7s on scrubs (scrub: 2).
+        //   "(min-width: 769px)": function() {
 
-            let desktopTL = gsap.timeline({
-              scrollTrigger: {
-                trigger: ".marquee",
-                start: "+40% bottom",
-                scrub: true,
-              }
-            });
+        //     let desktopTL = gsap.timeline({
+        //       scrollTrigger: {
+        //         trigger: ".marquee",
+        //         start: "+40% bottom",
+        //         scrub: true,
+        //       }
+        //     });
 
-            desktopTL.to(".first", {duration: 2, xPercent: -150})
-          }
-        });
+        //     desktopTL.to(".first", {duration: 2, xPercent: -150})
+        //   }
+        // });
 
         
 
